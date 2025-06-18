32b6e40c5ef6b5295f1a92a476df387f9b07d4e3
Douglas Rafael <doodohrafael@gmail.com>
2025-06-18 07:55:15 -0300

    layout do menu e do conteúd o principal melhorado.

--- src/app/pages/register-product/register-product.component.ts
+++ src/app/pages/register-product/register-product.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { SharedImports } from '../../shared/shared-imports';
+import { FormControl, FormGroup } from '@angular/forms';
+import { IftaLabelModule } from 'primeng/iftalabel';
 
 @Component({
   selector: 'os-register-product',
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, SharedImports, IftaLabelModule],
   standalone: true,
   templateUrl: './register-product.component.html',
   styleUrl: './register-product.component.scss'
 })
-export class RegisterProductComponent {
-    
+export class RegisterProductComponent implements OnInit {
+
+  public formRegisterProduct: FormGroup = new FormGroup({
+      name: new FormControl(''),
+    });;
+
+  ngOnInit() {
+  }
 }
+
