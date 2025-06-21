a11ec61ebde222a40c53d6abd21a1fbd664e6cd1
Douglas Rafael <doodohrafael@gmail.com>
2025-06-20 23:07:59 -0300

    melhoria na tela de cadastrar produto

--- src/app/pages/register-product/register-product.component.ts
+++ src/app/pages/register-product/register-product.component.ts
@@ -2,22 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { SharedImports } from '../../shared/shared-imports';
 import { FormControl, FormGroup } from '@angular/forms';
+import { FloatLabel } from 'primeng/floatlabel';
 import { IftaLabelModule } from 'primeng/iftalabel';
+import { Select } from 'primeng/select';
+import { Toolbar } from 'primeng/toolbar';
 
 @Component({
   selector: 'os-register-product',
-  imports: [DashboardComponent, SharedImports, IftaLabelModule],
+  imports: [DashboardComponent, SharedImports, FloatLabel, IftaLabelModule, Select, Toolbar],
   standalone: true,
   templateUrl: './register-product.component.html',
   styleUrl: './register-product.component.scss'
 })
 export class RegisterProductComponent implements OnInit {
 
+  categories: any[] = [];
+  items: any[] = [];
+
   public formRegisterProduct: FormGroup = new FormGroup({
-      name: new FormControl(''),
-    });;
+    name: new FormControl(''),
+    sku: new FormControl(''),
+    unitValue: new FormControl(''),
+    quantityStock: new FormControl(''),
+    category: new FormControl(null),
+    subCategory: new FormControl(null),
+  });;
 
   ngOnInit() {
+    this.items = [
+      {
+        label: 'Update',
+        icon: 'pi pi-refresh'
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-times'
+      }
+    ];
+
+    this.categories = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
   }
+
 }
 
