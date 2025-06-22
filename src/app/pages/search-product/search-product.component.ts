a5b0893fa4cb46aa67bdc2c28b0d509bc644d30f
Douglas Rafael <doodohrafael@gmail.com>
2025-06-22 10:59:33 -0300

    form added

--- src/app/pages/search-product/search-product.component.ts
+++ src/app/pages/search-product/search-product.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { SharedImports } from '../../shared/shared-imports';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import { FormControl, FormGroup } from '@angular/forms';
+import { IftaLabelModule } from 'primeng/iftalabel';
 
 @Component({
   selector: 'os-search-product',
   standalone: true,
-  imports: [DashboardComponent, SharedImports],
+  imports: [DashboardComponent, SharedImports, IftaLabelModule],
   templateUrl: './search-product.component.html',
   styleUrl: './search-product.component.scss'
 })
 export class SearchProductComponent {
+
+  public formSearchProduct: FormGroup = new FormGroup({
+    sku: new FormControl(''),
+    name: new FormControl(''),
+  });;
 
 }
